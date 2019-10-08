4cfc6fd82367802a7d80175e26a7d71b7eaa05e4
Andrea <andrea@villegas.com>
2019-10-07 21:00:53 -0400

    QUESTION 2 FINISHED

--- BonusAssignment/Question2_WeeklyCalendar.aspx.cs
+++ BonusAssignment/Question2_WeeklyCalendar.aspx.cs
@@ -17,70 +17,52 @@ namespace BonusAssignment
 
                 if (Page.IsValid)
                 {
-                    //DETERMINE WHICH GIVEN DAY IS A WORK DAY
+                   
+
+                    //VARIABLE DECLARATION
+                    string DayofTheWeek;// string to determine what day of the week is the given date
+                    int NumberDayofTheWeek; //int that represents the day of the week i.e Monday = 0
+                    string AddtoCalendar = ""; // string to store every line in the calendar
+                    string Activity = ""; //string writes whether it is time to work or have fun 
+                    List<string> ListDaysofTheWeek = new List<string>();//List stores days of the week
+                    ListDaysofTheWeek.Add("Monday");
+                    ListDaysofTheWeek.Add("Tuesday");
+                    ListDaysofTheWeek.Add("Wednesday");
+                    ListDaysofTheWeek.Add("Thursday");
+                    ListDaysofTheWeek.Add("Friday");
+                    ListDaysofTheWeek.Add("Saturday");
+                    ListDaysofTheWeek.Add("Sunday");
+
+                  
+                    AddtoCalendar += work_schedule_summary.InnerHtml = "Your October schedule is as follows <br>";
 
                     //TASK MATCH THE DAYS OF THE WEEK WITH THE GIVEN DAYS OF THE MONTH
-                    string DayofTheWeek = "";// string to determine what day of the week is the given date
-                    string AddtoCalendar = ""; // string to store every line in the calendar
-                    string Activity = ""; //string writes wether it is time to work or have fun 
-                    List<string> WorkDaysSelected = new List<string>();
-
-                    //DETERMINE WHICH DAYs ARE WORK DAYS
-
-
-                    foreach (ListItem day in work_days.Items)
-                    {
-                        if (day.Selected)
-                        {
-                            WorkDaysSelected.Add(day.Value);
-                        }
-                    }
-
-                    work_schedule_summary.InnerHtml = "Your October schedule is as follows <br>";
                     for (int i = 1; i <= 31; i++)
                     {
+                        NumberDayofTheWeek = i % 7;
+                        DayofTheWeek = ListDaysofTheWeek.ElementAt(NumberDayofTheWeek);
 
-                        if (i % 7 == 0)
+                        //TASK: DETERMINE THE DAYS THAT ARE WORK DAYS
+                        foreach (ListItem day in work_days.Items)
                         {
-                            DayofTheWeek = "Monday";
-
-                        }
-                        else if (i % 7 == 1)
-                        {
-                            DayofTheWeek = "Tuesday";
-
-                        }
-                        else if (i % 7 == 2)
-                        {
-                            DayofTheWeek = "Wednesday";
-
-                        }
-                        else if (i % 7 == 3)
-                        {
-                            DayofTheWeek = "Thursday";
-
-                        }
-                        else if (i % 7 == 4)
-                        {
-                            DayofTheWeek = "Friday";
-
-                        }
-                        else if (i % 7 == 5)
-                        {
-                            DayofTheWeek = "Saturday";
-
-                        }
-                        else if (i % 7 == 6)
-                        {
-                            DayofTheWeek = "Sunday";
+                            if(day.Value == DayofTheWeek)
+                            {
+                                if (day.Selected)
+                                {
+                                    Activity = "Time to work...";
+                                } else
+                                {
+                                    Activity = "Time to have fun!";
+                                }
+                            }
+                            
+               
                         }
 
-                        AddtoCalendar += "October " + i + " is a " + DayofTheWeek + "! <br>";
+                        //TASK: OUTPUT A MESSAGE THAT SAYS THE DATE AND THE ACTIVITY
+                        AddtoCalendar += "October " + i + " is a " + DayofTheWeek + "! " + Activity+ " <br>";
                     }
-                    work_schedule_summary.InnerHtml = AddtoCalendar;
-
-
-                    
+                    work_schedule_summary.InnerHtml = AddtoCalendar;  
 
                 }
 
